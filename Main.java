f93d01f706ff32b4b016fb657f7da76dc62e96ab
PRIYANSHU GOEL <priyanshugoel@PRIYANSHUs-MacBook-Air.local>
2023-09-29 02:28:26 +0530

    M-3

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.print("merged both the branches");
+        System.out.print("M-3");
     }
 }
